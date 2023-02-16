796a2a7dadc1add9213f75dba51c5917f75957d6
Hugues Mourice <mourice.hugues@gmail.com>
2023-02-16 10:04:47 +0100

    auto commit

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Image from "next/image";
-import SearchBar from "./SearchBar";
+import SearchBar from "./Layout/SearchBar";
+import Pokedex from "pokedex-promise-v2";
+const P = new Pokedex();
+
+function getPokemonNames() {
+  P.getPokemonsList()
+    .then(function (response) {
+      response.results.forEach((pokemon) => {
+        P.getResource([`api/v2/pokemon-species/${pokemon.name}`]).then(
+          (res) => {
+            console.log(res);
+          }
+        );
+      });
+    })
+    .catch(function (error) {
+      console.log("There was an ERROR: ", error);
+    });
+}
+
+function getMoveNames() {
+  P.getMovesList()
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log("There was an ERROR: ", error);
+    });
+}
 
 const Header = () => {
   return (
@@ -10,9 +38,10 @@ const Header = () => {
         <Image src="/umbreon-logo.png" alt="logo" width={200} height={100} />
         <h1>DevonDex</h1>
       </Box>
-      <SearchBar />     
+      <SearchBar />
+      <button onClick={getPokemonNames}>attrapez-les tous!</button>
+      <button onClick={getMoveNames}>get moves</button>
     </header>
   );
 };
-
 export default Header;
---
+++ pages/Pokedex.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pokedex = () => {
+  return (
+<section>
+    <h1>Pokedex</h1>
+    
+    
+</section>  )
+}
+
+export default Pokedex
