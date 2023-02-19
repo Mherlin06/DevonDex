ec906635d78e009840257f07e95d68f9388623d5
Hugues Mourice <mourice.hugues@gmail.com>
2023-02-19 22:19:59 +0100

    change default sprite & corrected key issues

--- pages/Pokedex.jsx
+++ pages/Pokedex.jsx
@@ -7,7 +7,8 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import pokedex from "../src/pokedex";
-import TypeIcon from "../components/icons/TypeIcon";
+import TypeIcon from "../components/Icons/TypeIcon";
+import Image from "next/image";
 
 const Pokedex = () => {
   return (
@@ -15,16 +16,27 @@ const Pokedex = () => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell align="center">Pokedex National</TableCell>
+            <TableCell align="center">
+              Pokedex <br /> National
+            </TableCell>
             <TableCell align="center">Sprite</TableCell>
             <TableCell align="center">Nom</TableCell>
             <TableCell align="center">Types</TableCell>
-            <TableCell align="center">Capacités Spéciales</TableCell>
+            <TableCell align="center">
+              Capacités
+              <br /> Spéciales
+            </TableCell>
             <TableCell align="center">HP</TableCell>
             <TableCell align="center">Attaque</TableCell>
             <TableCell align="center">Defense</TableCell>
-            <TableCell align="center">Attaque Spéciale</TableCell>
-            <TableCell align="center">Defense Spéciale</TableCell>
+            <TableCell align="center">
+              Attaque <br />
+              Spéciale
+            </TableCell>
+            <TableCell align="center">
+              Defense
+              <br /> Spéciale
+            </TableCell>
             <TableCell align="center">Vitesse</TableCell>
           </TableRow>
         </TableHead>
@@ -34,19 +46,44 @@ const Pokedex = () => {
               key={pokemon.id}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <TableCell component="th" scope="pokemon">
+              <TableCell component="th" scope="pokemon" align="center">
                 {pokemon.pokedexEntry}
               </TableCell>
-              <TableCell align="center">{pokemon.sprite}</TableCell>
               <TableCell align="center">
-                {pokemon.frenchName} <br></br> {pokemon.name}
+                {pokemon.miniSprite ? (
+                  <Image
+                    src={pokemon.miniSprite}
+                    alt={pokemon.frenchName}
+                    width={75}
+                    height={75}
+                  />
+                ) : (
+                  <Image
+                    src={"/default-mini-sprite.png"}
+                    alt={"default"}
+                    width={75}
+                    height={75}
+                  />
+                )}
+              </TableCell>
+              <TableCell align="center">
+                {pokemon.frenchName} <br /> {pokemon.name}
               </TableCell>
               <TableCell align="center">
                 {pokemon.types.map((type) => (
-                  <TypeIcon type={type} key={type} />
+                  <div key={type}>
+                    <TypeIcon type={type} />
+                    <br />
+                  </div>
                 ))}
               </TableCell>
-              <TableCell align="center">{pokemon.abilities}</TableCell>
+              <TableCell align="center">
+                <ul>
+                  {pokemon.abilities.map((ability, index) => (
+                    <li key={ability + index}>{ability}</li>
+                  ))}
+                </ul>
+              </TableCell>
               <TableCell align="center">{pokemon.stats.hp}</TableCell>
               <TableCell align="center">{pokemon.stats.attack}</TableCell>
               <TableCell align="center">{pokemon.stats.defense}</TableCell>
